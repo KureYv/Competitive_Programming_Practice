424ba6c62dee7e5d7316c1b4cdd91fb9068586a8
Matthew <matthewlee031205@gmail.com>
2020-11-04 11:26:36 -0500

    Update CCCS1.py

--- DMOJ/CCCS1.py
+++ DMOJ/CCCS1.py
@@ -4,7 +4,11 @@ def main():
     x,y = stdin.readline().split()
     x = int(x)
     y = int(y)
-    volume = (x*y)/2
+    volume = str(int((x*y)/2))
+    if (x%2 != 0 and y%2 != 0):
+        volume = volume+".5"
+    else:
+        volume = volume+".0"
     return volume
 
 
